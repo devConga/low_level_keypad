ffeaab567793c840a42977681e0a7816ff006d7f
Conga <alejandro.combasanz2004@gmail.com>
2023-06-11 19:40:33 -0300

    El programa puede identificar la columna del boton presionado

--- src/main.cpp
+++ src/main.cpp
@@ -14,35 +14,78 @@ char matriz [4][4]={   //La distribucion del teclado
 };
 
 
-// DDRB &= (0 << PB0) & (0 << PB1) & (0 << PB2) & (0 << PB3);
-// DDRD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);
+DDRB &= ~(1 << PB0) & ~(1 << PB1) & ~(1 << PB2) & ~(1 << PB3);        // PB0, PB1, PB2 y PB3 seteados a INPUT 
+DDRD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);            // PD2, PD3, PD4 y PD5 seteados a OUTPUT
 
 
-// PORTB |= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3);
+PORTD &= ~(1 << PD2) & ~(1 << PD3) & ~(1 << PD4) & ~(1 << PD5);
+PORTB |= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3);           // Se habilitan los pull-up resistors de todos los INPUT
 
 
-
-DDRB |= (1 << PB0); //PB0 en Output
-DDRB &= ~(1 << PB1); //PB1 en Input
-
-
-// PORTB &= ~(1 << PB1); //Sin pull-up resistor
-PORTB |= (1 << PB1); //Con internal pull-up resistor
-
-int i=1;
 
 while(1){
 
-  if(!(PINB & (1 << PB1))){        //Evalua si PB1 esta en LOW (0) y lo invierte con un !, asi entra al if
+  if(!(PINB & (1 << PB0))){                        // Evalua si se presiona algun boton de la 1ra columna
+    Serial.println("1C");
 
-    Serial.print("Prendido ");
-    Serial.println(i);
-    i++;
+
+
+
+
+
+
 
   }
 
-  
+  if(!(PINB & (1 << PB1))){                       // Evalua si se presiona algun boton de la 2da columna
+    Serial.println("2C");
+  }
+
+  if(!(PINB & (1 << PB2))){                       // Evalua si se presiona algun boton de la 3ra columna
+    Serial.println("3C");
+  }
+
+  if(!(PINB & (1 << PB3))){                       // Evalua si se presiona algun boton de la 4ta columna
+    Serial.println("4C");
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------Esta es la prueba del boton------------------------------------------------------------------
+
+// DDRB |= (1 << PB0); //PB0 en Output
+// DDRB &= ~(1 << PB1); //PB1 en Input
+
+
+// // PORTB &= ~(1 << PB1); //Sin pull-up resistor
+// PORTB |= (1 << PB1); //Con internal pull-up resistor
+
+// int i=1;
+
+// while(1){
+
+//   if(!(PINB & (1 << PB1))){        //Evalua si PB1 esta en LOW (0) y lo invierte con un !, asi entra al if
+
+//     Serial.print("Prendido ");
+//     Serial.println(i);
+//     i++;
+
+//   }
+
+  
+// }
 
 
 }
