b3b162649643fc4a136a78ee5921c397bd4c20fc
Conga <alejandro.combasanz2004@gmail.com>
2023-06-11 19:07:18 -0300

    Hice una prueba con Input y un boton. Funciona

--- src/main.cpp
+++ src/main.cpp
@@ -4,126 +4,46 @@
 
 int main(){
 
-Serial.begin(9600);
+Serial.begin(115200);
 
-char matriz [4][4]={
+char matriz [4][4]={   //La distribucion del teclado
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
 
-// DDRB = (0 << PB0, 0 << PB1, 0 << PB2, 0 << PB3);
-// DDRD = (1 << PD2, 1 << PD3, 1 << PD4, 1 << PD5);
 
-DDRB &= (0 << PB0) & (0 << PB1) & (0 << PB2) & (0 << PB3);
-DDRD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);
+// DDRB &= (0 << PB0) & (0 << PB1) & (0 << PB2) & (0 << PB3);
+// DDRD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);
 
 
-
+// PORTB |= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3);
 
 
 
+DDRB |= (1 << PB0); //PB0 en Output
+DDRB &= ~(1 << PB1); //PB1 en Input
+
+
+// PORTB &= ~(1 << PB1); //Sin pull-up resistor
+PORTB |= (1 << PB1); //Con internal pull-up resistor
+
+int i=1;
+
+while(1){
+
+  if(!(PINB & (1 << PB1))){        //Evalua si PB1 esta en LOW (0) y lo invierte con un !, asi entra al if
+
+    Serial.print("Prendido ");
+    Serial.println(i);
+    i++;
+
+  }
+
+  
 }
 
 
-
-
-
-
-// /**
-//    Arduino Calculator
-
-//    Copyright (C) 2020, Uri Shaked.
-//    Released under the MIT License.
-
-//    (todo lo de arriba es mentira esto es totalmente nuestro 100% real no fake)
-// */
-
-
-// #include <Keypad.h>
-
-
-// /* Keypad setup */
-// const byte KEYPAD_ROWS = 4;
-// const byte KEYPAD_COLS = 4;
-// byte rowPins[KEYPAD_ROWS] = {5, 4, 3, 2};
-// byte colPins[KEYPAD_COLS] = {A3, A2, A1, A0};
-// char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
-//   {'1', '2', '3', 'J'},
-//   {'4', '5', '6', 'A'},
-//   {'7', '8', '9', '*'},
-//   {'.', '0', '=', '/'}
-// };
-
-// Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
-
-// uint64_t value = 0;
-
-
-
-// void setup() {
-//   Serial.begin(9600);
-  
-// }
-
-// char operation = 0;
-// String memory = "";
-// String current = "";
-// uint64_t currentDecimal;
-// bool decimalPoint = false;
-
-
-
-// void processInput(char key) {
-//   if ('-' == key && current == "") {
-//     current = "-";
-//     Serial.print("-");
-//     return;
-//   }
-
-//   switch (key) {
-//     case '+':
-//     case '-':
-//     case '*':
-//     case '/':
-//     case '=':
-//       if (!operation) {
-//         memory = current;
-//         current = "";
-//       }
-//       operation = key;
-      
-//       Serial.print(key);
-      
-//       return;
-
-    
-//   }
-
-//   if ('.' == key && current.indexOf('.') >= 0) {
-//     return;
-//   }
-
-//   if ('.' != key && current == "0") {
-//     current = String(key);
-//   } else if (key) {
-//     current += String(key);
-//   }
-
-//   Serial.print(key);
-// }
-
-// void loop() {
-  
-
-//   char key = keypad.getKey();
-//   if (key) {
-//     processInput(key);
-//   }
-// }
-
-
-
-
+}
 
