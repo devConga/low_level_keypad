d3d2659e122440580362e4b5d19e7103f828afe7
martualarc <martina.alarcon029@gmail.com>
2023-06-13 19:25:17 -0300

    evalua columnas dentro de una funcion

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 #include <Arduino.h>
 
 int evaluarFila();
+int evaluarColumna();
 
 void columnasInput();
 void columnasOutput();
 
 int main(){
 
-Serial.begin(115200);
+  Serial.begin(9600);
 
-char matriz [4][4]={   //La distribucion del teclado
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
-
+  char matriz [4][4]={   //La distribucion del teclado
+    {'1','2','3','A'},
+    {'4','5','6','B'},
+    {'7','8','9','C'},
+    {'*','0','#','D'}
+  };
 
   columnasInput();
 
@@ -26,29 +26,8 @@ char matriz [4][4]={   //La distribucion del teclado
   while(1){
 
     
-    if(!(PINB & (1 << PB0))){                        // Evalua si se presiona algun boton de la 1ra columna
-      // Serial.println("1C");
-      columna=0;
-      fila=evaluarFila();
-    }
-
-    if(!(PINB & (1 << PB1))){                       // Evalua si se presiona algun boton de la 2da columna
-      // Serial.println("2C");
-      columna=1;
-      fila=evaluarFila();
-    }
-
-    if(!(PINB & (1 << PB2))){                       // Evalua si se presiona algun boton de la 3ra columna
-      // Serial.println("3C");
-      columna=2;
-      fila=evaluarFila();
-    }
-
-    if(!(PINB & (1 << PB3))){                       // Evalua si se presiona algun boton de la 4ta columna
-      // Serial.println("4C");
-      columna=3;
-      fila=evaluarFila();
-    }
+    columna=evaluarColumna();  
+    fila=evaluarFila();
 
 
     if(columna>=0 && fila>=0){
@@ -64,9 +43,6 @@ char matriz [4][4]={   //La distribucion del teclado
   }
 
 }
-
-  
-
 
 void columnasInput(){
 
@@ -88,6 +64,27 @@ void columnasOutput(){
   PORTB &= ~(1 << PB0) & ~(1 << PB1) & ~(1 << PB2) & ~(1 << PB3);
   PORTD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);      
 
+}
+
+int evaluarColumna(){
+
+  while(1){
+    if(!(PINB & (1 << PB0))){                     // Evalua si se presiona algun boton de la 1ra columna
+      return 0;
+    }
+
+    if(!(PINB & (1 << PB1))){                       // Evalua si se presiona algun boton de la 2da columna
+      return 1;
+    }
+
+    if(!(PINB & (1 << PB2))){                       // Evalua si se presiona algun boton de la 3ra columna
+      return 2;
+    }
+
+    if(!(PINB & (1 << PB3))){                       // Evalua si se presiona algun boton de la 4ta columna
+      return 3;
+    }
+  }
 }
 
 int evaluarFila(){
@@ -151,6 +148,7 @@ int evaluarFila(){
 
   
 // }
+
 
 
 
